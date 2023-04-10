43681ab787bd7cac04ac1641d4b589dd4e0f0a2d
boxmancoder <isaacfrancisco305@gmail.com>
2023-04-09 21:42:43 -0400

    fixed appjava

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,31 +1,25 @@
 package com.h2;
 
-import java.util.Scanner;
-import java.util.Map;
-public class BestLoanRates {
-    public static final Map<Integer, Float> bestRates = Map.of
-            (1, 5.50f, 2, 3.45f, 3, 2.67f);
-
-    //void means it returns nothing.
-    public static void main(String [] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name);
-
-        System.out.println("Enter the loan term (in years)");
-        int loanTermInYears = scanner.nextInt();
-        float bestRate = getRates(loanTermInYears);
-        if(bestRate == 0.0f) {
-            System.out.println("No available rates for term: " + loanTermInYears + " years");
-        }else {
-            System.out.println("Best Available Rate: " + getRates(loanTermInYears) + "%");
-        }
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
-    public static float getRates( int loanTermInYears) {
-        if (bestRates.containsKey(loanTermInYears)) {
-            return bestRates.get(loanTermInYears);
+
+    public static int doubleTheNumber(int number) {
+
+        return 2 * number;
+    }
+    private static int add(int[] numbers) {
+        var sum = 0;
+        for(int number: numbers) {
+            sum += number;
         }
-        return 0.0f;
+        return sum;
     }
 }
