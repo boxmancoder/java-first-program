9ec4a8b4e3c65d4d582a75032df59d90a6037852
boxmancoder <isaacfrancisco305@gmail.com>
2023-04-09 20:40:29 -0400

    first commit

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -1,17 +1,35 @@
 package com.h2;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Map;
+import java.util.Scanner;
+
+public class BestLoanRates {
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
+
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermInYears);
+        if(bestRate == 0.0f) {
+            System.out.println("No available rates for term: " + loanTermInYears + " years");
+        } else {
+            System.out.println("Best Available Rate: " + bestRate + "%");
+        }
     }
 
-    public static int doubleTheNumber(int number) {
-        return -1;
+    public static float getRates(int loanTermInYears) {
+        if(bestRates.containsKey(loanTermInYears)) {
+            return bestRates.get(loanTermInYears);
+        }
+        return 0.0f;
     }
 }
